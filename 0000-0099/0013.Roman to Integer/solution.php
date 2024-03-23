7c77f289345969e91364602dac7050d8e135fed1
zmijewskik <kzmijewski@protonmail.com>
2024-03-23 22:27:52 +0100

    Add 13

---
+++ 0000-0099/0013.Roman to Integer/solution.php
@@ -0,0 +1,29 @@
+<?php
+class Solution {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        $s = strtr(
+            $s,
+            [
+                'M' => '1000,',
+                'CM' => '900,',
+                'D' => '500,',
+                'CD' => '400,',
+                'C' => '100,',
+                'XC' => '90,',
+                'L' => '50,',
+                'XL' => '40,',
+                'X' => '10,',
+                'IX' => '9,',
+                'V' => '5,',
+                'IV' => '4',
+                'I' => '1,'
+            ]
+        );
+        return array_sum(explode(',', $s));
+    }
+}
+?>
