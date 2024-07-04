2e5d55dfbfc67f7af7aad4f0d7ec7cb464e05a7d
zmijewskik <kzmijewski@protonmail.com>
2024-07-04 23:36:32 +0200

    Add 35

---
+++ 0000-0099/0035.Search Insert Position/solution.php
@@ -0,0 +1,26 @@
+<?php
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @param Integer $target
+   * @return Integer
+   */
+  function searchInsert($nums, $target) {
+      $left = 0;
+      $right = count($nums) - 1;
+
+      while($left <= $right){
+          $mid = floor(($right + $left) / 2);
+          if($nums[$mid] == $target){
+              return $mid;
+          }else if ($nums[$mid] < $target){
+              $left = $mid + 1;
+          }else{
+              $right = $mid - 1;
+          }
+      }
+      return $left;
+  }
+}
+>
